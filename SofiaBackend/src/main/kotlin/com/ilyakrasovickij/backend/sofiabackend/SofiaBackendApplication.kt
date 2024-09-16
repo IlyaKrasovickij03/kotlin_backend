dd9ed37c25bdc4f7999dfaed80f957925acc71c0
Илья Красовицкий <krasovitskii.i@ati.su>
2024-09-16 21:36:44 +0300

    initial commit

---
+++ SofiaBackend/src/main/kotlin/com/ilyakrasovickij/backend/sofiabackend/SofiaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.ilyakrasovickij.backend.sofiabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SofiaBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SofiaBackendApplication>(*args)
+}
---
+++ SofiaBackend/src/test/kotlin/com/ilyakrasovickij/backend/sofiabackend/SofiaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ilyakrasovickij.backend.sofiabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SofiaBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
